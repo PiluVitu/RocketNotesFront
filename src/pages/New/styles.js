df1f58e2fd3754ff3dc26ee80ed561266506dcd9
Paulo Victor Torres Silva <paulo.tspi@gmail.com>
2023-04-12 20:18:26 -0300

    style: create styles.js on new

---
+++ src/pages/New/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+
+  display: grid;
+  grid-template-rows: 10.5rem auto;
+  grid-template-areas: 
+  "header"
+  "content"
+  ;
+`
