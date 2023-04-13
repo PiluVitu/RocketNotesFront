60efa5613312b600cfd3e1299cc6965ce2a44068
Paulo Victor Torres Silva <paulo.tspi@gmail.com>
2023-04-12 23:39:32 -0300

    feat: |Container grid area defined && based css for Form complete

--- src/pages/New/styles.js
+++ src/pages/New/styles.js
@@ -6,8 +6,25 @@ export const Container = styled.div`
 
   display: grid;
   grid-template-rows: 10.5rem auto;
-  grid-template-areas: 
-  "header"
-  "content"
-  ;
+  grid-template-areas:
+    'header'
+    'content';
+`
+
+export const Form = styled.form`
+  max-width: 55rem;
+  margin: 3.8rem auto;
+
+  > header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-bottom: 3.6rem;
+
+    a {
+      font-size: 2rem;
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+  }
 `
