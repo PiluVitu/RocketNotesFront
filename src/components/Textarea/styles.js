1f4aa9e9eb1135ae4ed7f449e38fd5160e255303
Paulo Victor Torres Silva <paulo.tspi@gmail.com>
2023-04-13 00:46:22 -0300

    feat: Textarea stylized

--- src/components/Textarea/styles.js
+++ src/components/Textarea/styles.js
@@ -1,5 +1,22 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const Container = styled.textarea`
-  
+  width: 100%;
+  height: 15rem;
+  border-radius: 1rem;
+  border: none;
+
+  resize: none;
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+  padding: 1.6rem;
+
+  margin-bottom: 0.8rem;
+
+  color: ${({ theme }) => theme.COLORS.WHITE};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.COLORS.GRAY_300};
+  }
 `
