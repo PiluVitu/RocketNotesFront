f900da585616c69db4654af0d29bdbd729660ebc
Paulo Victor Torres Silva <paulo.tspi@gmail.com>
2023-04-15 18:37:15 -0300

    feat: add style for .tags div and ajust page flow to matein header fixed

--- src/pages/New/styles.js
+++ src/pages/New/styles.js
@@ -9,6 +9,20 @@ export const Container = styled.div`
   grid-template-areas:
     'header'
     'content';
+
+    > main {
+      grid-area: content;
+      overflow-y: auto;
+    }
+
+  .tags {
+    display: flex;
+
+    align-items: center;
+    justify-content: space-between;
+
+    flex-wrap: wrap;
+  } 
 `
 
 export const Form = styled.form`
