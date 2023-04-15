27303ae22ee57fe83186850ab8f967e55306c101
Paulo Victor Torres Silva <paulo.tspi@gmail.com>
2023-04-15 18:33:11 -0300

    feat: component styled and functional

---
+++ src/components/NoteItem/styles.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+
+  background: ${({ theme, isNew }) =>
+    isNew ? 'transparent' : theme.COLORS.BACKGROUND_900};
+
+  color: ${({ theme }) => theme.COLORS.GRAY_300};
+
+  border: ${({ theme, isNew }) =>
+    isNew ? `1px dashed ${theme.COLORS.GRAY_300}` : 'none'};
+
+  margin-bottom: 0.8rem;
+  border-radius: 1rem;
+  padding-right: 1.6rem;
+
+  > button {
+    background: none;
+
+    border: none;
+  }
+
+  .buttonDelete {
+    color: ${({theme})=> theme.COLORS.RED}
+  }
+  .buttonAdd {
+    color: ${({theme})=> theme.COLORS.ORANGE}
+  }
+
+  > input {
+    height: 5.6rem;
+    width: 100%;
+
+    padding: 1.2rem;
+
+
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    background: transparent;
+
+    border: none;
+
+    &::placeholder {
+      ${({ theme }) => theme.COLORS.GRAY_300}
+    }
+  }
+`
